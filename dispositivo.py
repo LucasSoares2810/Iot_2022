49d25f78f80ed5b47415a48f651bd27e4bd78c3e
LucasSoares2810 <lucaslebre138@gmail.com>
2022-09-25 22:07:04 -0300

    Adicionando aquecedor

--- dispositivo.py
+++ dispositivo.py
@@ -15,23 +15,30 @@ client = mqtt.Client(client_id)
 client.username_pw_set(user, password)
 client.connect(server, port)
 
+
 client.on_message = mensagem
 client.subscribe('v1/' + user + '/things/' + client_id + '/cmd/2')
 client.loop_start()
 
-
+x = temperatura()
+botao = aquecedor('')
 while True:
-    temp = temperatura()
-    if temp <= 30:
-        while temp <= 30:
+    temp = x
+    if temp <= 35:
+        while temp <= 35:
             temp += 1
             client.publish('v1/' + user + '/things/' + client_id + '/data/0', temp)
             time.sleep(2)
+        x = temp
+    elif temp >= 35:
+        while temp >= 30:
+            temp -= 1
+            client.publish('v1/' + user + '/things/' + client_id + '/data/0', temp)
+            time.sleep(2)
+        x = temp
     else:
         client.publish('v1/' + user + '/things/' + client_id + '/data/0', temp)
 
     client.publish('v1/' + user + '/things/' + client_id + '/data/1', umidade())
-
     time.sleep(10)
 
-# client.disconnect()
